c36925aedaeef4e37a3ffc78ef49accabae49034
Matthieu Lefebvre <ml15@princeton.edu>
2015-03-23 10:20:34 -0400

    Adds missing underscores

--- src/ASDF_f.c
+++ src/ASDF_f.c
@@ -8,11 +8,11 @@
  *  Wrappers for ASDF_init                                *
  **********************************************************/
 
-void ASDF_initialize_hdf5_f(int *err) {
+void ASDF_initialize_hdf5_f_(int *err) {
   *err = ASDF_initialize_hdf5();
 }
 
-void ASDF_finalize_hdf5_f(int *err) {
+void ASDF_finalize_hdf5_f_(int *err) {
   *err = ASDF_finalize_hdf5();
 }
 
@@ -20,43 +20,43 @@ void ASDF_finalize_hdf5_f(int *err) {
  *  Wrappers for ASDF_write                               *
  **********************************************************/
 
-void ASDF_create_new_file_f(char *filename, MPI_Comm *comm, int *err) {
+void ASDF_create_new_file_f_(char *filename, MPI_Comm *comm, int *err) {
   *err = ASDF_create_new_file(filename, *comm);
 }
 
-void ASDF_write_string_attribute_f(hid_t *dataset_id, char *attr_name, 
-                                   char *attr_value, int *err) {
+void ASDF_write_string_attribute_f_(hid_t *dataset_id, char *attr_name, 
+                                    char *attr_value, int *err) {
   *err = ASDF_write_string_attribute(*dataset_id, attr_name, attr_value);
 }
 
-void ASDF_write_auxiliary_data_f(hid_t *loc_id, int *err) {
+void ASDF_write_auxiliary_data_f_(hid_t *loc_id, int *err) {
   *err = ASDF_write_auxiliary_data(*loc_id);
 }
 
-void ASDF_write_provenance_data_f(hid_t *loc_id, int *err) {
+void ASDF_write_provenance_data_f_(hid_t *loc_id, int *err) {
   *err = ASDF_write_provenance_data(*loc_id);
 }
 
-void ASDF_write_quakeml_f(hid_t *loc_id, char *quakeml_string, int *err) {
+void ASDF_write_quakeml_f_(hid_t *loc_id, char *quakeml_string, int *err) {
   *err = ASDF_write_quakeml(*loc_id, quakeml_string);
 }
 
-void ASDF_define_waveforms_f(hid_t *loc_id, int *num_waveforms, int *nsamples,
-                             int *start_time, double *sampling_rate, 
-                             char *event_name, char **waveform_names,
-                             int *groups, int *data_id, int *err) {
-  *err = ASDF_define_waveforms(*loc_id, *num_waveforms, *nsamples, 
+void ASDF_define_waveforms_f_(hid_t *loc_id, int *num_waveforms, int *nsamples,
+                              int *start_time, double *sampling_rate, 
+                              char *event_name, char **waveform_names,
+                              int *groups, int *data_id, int *err) {
+  *err = ASDF_define_waveforms_(*loc_id, *num_waveforms, *nsamples, 
                                *start_time, *sampling_rate, 
                                event_name, waveform_names,
                                groups, data_id);
 }
 
-void ASDF_write_full_waveform_f(hid_t *data_id, float *waveform, int *err) {
+void ASDF_write_full_waveform_f_(hid_t *data_id, float *waveform, int *err) {
   *err = ASDF_write_full_waveform(*data_id, waveform);
 }
 
-void ASDF_write_partial_waveform_f(hid_t *data_id, float *waveform,
-                                   int *offset, int *nsamples, int *err) {
+void ASDF_write_partial_waveform_f_(hid_t *data_id, float *waveform,
+                                    int *offset, int *nsamples, int *err) {
   *err = ASDF_write_partial_waveform(*data_id, waveform,
                                    *offset, *nsamples);
 }
